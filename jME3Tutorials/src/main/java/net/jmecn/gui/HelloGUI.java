2e4f3b64119d63039e03b0b7be113741ffb61416
yan <115050813@qq.com>
2017-05-05 18:33:50 +0800

    为第十章写的例子。

---
+++ jME3Tutorials/src/main/java/net/jmecn/gui/HelloGUI.java
@@ -0,0 +1,21 @@
+package net.jmecn.gui;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.texture.Texture;
+
+public class HelloGUI extends SimpleApplication {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public void simpleInitApp() {
+		Texture tex = assetManager.loadTexture("Interface/Gui/pic.png");
+
+	}
+
+
+}
