16e851f9fdc4ec275d9c3cafe5e30d6bc45ad505
yan <115050813@qq.com>
2017-05-09 19:44:12 +0800

    将英文注释翻译成中文注释。

--- jME3Tutorials/src/main/java/net/jmecn/HelloAudio.java
+++ jME3Tutorials/src/main/java/net/jmecn/HelloAudio.java
@@ -13,11 +13,18 @@ import com.jme3.scene.shape.Box;
 
 /**
  * 播放3D音效
+ * 
  * @author yanmaoyuan
  *
  */
 public class HelloAudio extends SimpleApplication {
 
+	public static void main(String[] args) {
+		// 启动程序
+		HelloAudio app = new HelloAudio();
+		app.start();
+	}
+	
 	/**
 	 * 枪声
 	 */
@@ -27,68 +34,90 @@ public class HelloAudio extends SimpleApplication {
 	 */
 	private AudioNode audioNature;
 
-	public static void main(String[] args) {
-		HelloAudio app = new HelloAudio();
-		app.start();
-	}
+	/**
+	 * 开枪
+	 */
+	private final static String SHOOT = "Shoot";
 
 	@Override
 	public void simpleInitApp() {
 		flyCam.setMoveSpeed(40);
 
 		/**
-		 * 制作一个蓝色的小方块，用它来表示3D音源的位置。
+		 * 制作一个蓝色的小方块，用来表示3D音源的位置。
 		 */
-	    Geometry player = new Geometry("Player", new Box(1, 1, 1));
-	    Material mat1 = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
-	    mat1.setColor("Color", ColorRGBA.Blue);
-	    player.setMaterial(mat1);
-	    rootNode.attachChild(player);
+		Geometry geom = new Geometry("Player", new Box(1, 1, 1));
+		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Blue);
+		geom.setMaterial(mat);
+		rootNode.attachChild(geom);
 
-	    /** custom init methods, see below */
-	    initKeys();
-	    initAudio();
+		/**
+		 * 初始化用户输入
+		 */
+		initKeys();
+		
+		/**
+		 * 初始化音效
+		 */
+		initAudio();
 
 	}
 
-	/** We create two audio nodes. */
-	  private void initAudio() {
-	    /* gun shot sound is to be triggered by a mouse click. */
-	    audioGun = new AudioNode(assetManager, "Sound/Effects/Gun.wav", DataType.Buffer);
-	    audioGun.setPositional(false);
-	    audioGun.setLooping(false);
-	    audioGun.setVolume(2);
-	    rootNode.attachChild(audioGun);
+	/**
+	 * 保持Listener和Camera同步移动，这样才能感受到3D音效。
+	 */
+	@Override
+	public void simpleUpdate(float tpf) {
+		listener.setLocation(cam.getLocation());
+		listener.setRotation(cam.getRotation());
+	}
 
-	    /* nature sound - keeps playing in a loop. */
-	    audioNature = new AudioNode(assetManager, "Sound/Environment/Ocean Waves.ogg", DataType.Stream);
-	    audioNature.setLooping(true);  // activate continuous playing
-	    audioNature.setPositional(true);
-	    audioNature.setVolume(3);
-	    rootNode.attachChild(audioNature);
-	    audioNature.play(); // play continuously!
-	  }
+	/**
+	 * 创建两个AudioNode作为音源，并添加到场景中。。
+	 */
+	private void initAudio() {
+		/**
+		 * 创建一个“枪声”音源，用户点击鼠标时会发出枪声。
+		 */
+		audioGun = new AudioNode(assetManager, "Sound/Effects/Gun.wav", DataType.Buffer);
+		audioGun.setLooping(false);// 禁用循环播放
+		audioGun.setPositional(false);// 设置为非定位音源，玩家无法通过耳机辨别音源的位置。常用于背景音乐。
+		audioGun.setVolume(2);
+		// 将音源添加到场景中
+		rootNode.attachChild(audioGun);
 
-	  /** Declaring "Shoot" action, mapping it to a trigger (mouse left click). */
-	  private void initKeys() {
-	    inputManager.addMapping("Shoot", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
-	    inputManager.addListener(actionListener, "Shoot");
-	  }
+		/**
+		 * 创建一个自然音效（海潮声），这个音源会一直循环播放。
+		 */
+		audioNature = new AudioNode(assetManager, "Sound/Environment/Ocean Waves.ogg", DataType.Stream);
+		audioNature.setLooping(true); // 循环播放
+		audioNature.setPositional(true);// 设置为定位音源，这将产生3D音效，玩家能够通过耳机来辨别音源的位置。
+		audioNature.setVolume(3);// 音量
+		// 将音源添加到场景中
+		rootNode.attachChild(audioNature);
+		
+		audioNature.play(); // 持续播放
+	}
 
-	  /** Defining the "Shoot" action: Play a gun sound. */
-	  private ActionListener actionListener = new ActionListener() {
-	    @Override
-	    public void onAction(String name, boolean keyPressed, float tpf) {
-	      if (name.equals("Shoot") && !keyPressed) {
-	        audioGun.playInstance(); // play each instance once!
-	      }
-	    }
-	  };
+	/**
+	 * 定义“开枪(SHOOT)”动作，用户点击鼠标左键时触发此动作。
+	 */
+	private void initKeys() {
+		inputManager.addMapping(SHOOT, new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+		inputManager.addListener(actionListener, SHOOT);
+	}
 
-	  /** Move the listener with the a camera - for 3D audio. */
-	  @Override
-	  public void simpleUpdate(float tpf) {
-	    listener.setLocation(cam.getLocation());
-	    listener.setRotation(cam.getRotation());
-	  }
+	/**
+	 * 定义一个监听器，用来处理开枪动作，当玩家点击鼠标左键时发出枪声。
+	 */
+	private ActionListener actionListener = new ActionListener() {
+		@Override
+		public void onAction(String name, boolean isPressed, float tpf) {
+			if (SHOOT.equals(name) && isPressed) {
+				audioGun.playInstance(); // 只播放一次
+			}
+		}
+	};
+
 }
