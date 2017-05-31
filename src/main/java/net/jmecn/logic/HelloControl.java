8398a6bb78735c8723620fe8cbbe8631a906f0f4
yan <115050813@qq.com>
2017-06-01 00:42:55 +0800

    Use control instead of AbstractControl

--- src/main/java/net/jmecn/logic/HelloControl.java
+++ src/main/java/net/jmecn/logic/HelloControl.java
@@ -17,30 +17,29 @@ import com.jme3.scene.shape.Box;
  */
 public class HelloControl extends SimpleApplication {
 
-	@Override
-	public void simpleInitApp() {
-		cam.setLocation(new Vector3f(3.3435764f, 3.7595856f, 6.611723f));
-		cam.setRotation(new Quaternion(-0.05573249f, 0.9440857f, -0.23910178f, -0.22006002f));
-		
-		// 创建一个方块
-		Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		Geometry spatial = new Geometry("Box", new Box(1, 1, 1));
-		spatial.setMaterial(mat);
-		
-		// 添加控件
-		spatial.addControl(new FloatControl());
-		spatial.addControl(new RotateControl(FastMath.PI));
-		
-		rootNode.attachChild(spatial);
-		
-		// 添加光源
-		rootNode.addLight(new DirectionalLight(new Vector3f(-1, -2, -3)));
-	}
-	
-	public static void main(String[] args) {
-		// 启动
-		HelloControl app = new HelloControl();
-		app.start();
-	}
+    @Override
+    public void simpleInitApp() {
+        cam.setLocation(new Vector3f(3.3435764f, 3.7595856f, 6.611723f));
+        cam.setRotation(new Quaternion(-0.05573249f, 0.9440857f, -0.23910178f, -0.22006002f));
+
+        // 创建一个方块
+        Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        Geometry spatial = new Geometry("Box", new Box(1, 1, 1));
+        spatial.setMaterial(mat);
+
+        // 添加控件
+        spatial.addControl(new RotateControl(FastMath.PI));
+
+        rootNode.attachChild(spatial);
+
+        // 添加光源
+        rootNode.addLight(new DirectionalLight(new Vector3f(-1, -2, -3)));
+    }
+
+    public static void main(String[] args) {
+        // 启动
+        HelloControl app = new HelloControl();
+        app.start();
+    }
 
 }
--- src/main/java/net/jmecn/logic/RotateControl.java
+++ src/main/java/net/jmecn/logic/RotateControl.java
@@ -1,8 +1,13 @@
 package net.jmecn.logic;
 
+import java.io.IOException;
+
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
-import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.Control;
 
 /**
  * 让模型绕Y轴以固定速率旋转
@@ -10,21 +15,45 @@ import com.jme3.scene.control.AbstractControl;
  * @author yanmaoyuan
  *
  */
-public class RotateControl extends AbstractControl {
+public class RotateControl implements Control {
 
-	// 旋转速度：每秒180°
-	private float rotateSpeed = 1f;
-	
-	public RotateControl(float rotateSpeed) {
-		this.rotateSpeed = rotateSpeed;
-	}
-	
-	@Override
-	protected void controlUpdate(float tpf) {
-		spatial.rotate(0, tpf * rotateSpeed, 0);
-	}
+    private Spatial spatial;
 
-	@Override
-	protected void controlRender(RenderManager rm, ViewPort vp) {}
+    // 旋转速度：每秒180°
+    private float rotateSpeed = 1f;
 
+    public RotateControl(float rotateSpeed) {
+        this.rotateSpeed = rotateSpeed;
+    }
+
+    @Override
+    public void setSpatial(Spatial spatial) {
+        this.spatial = spatial;
+    }
+
+    @Override
+    public void update(float tpf) {
+        spatial.rotate(0, tpf * rotateSpeed, 0);
+    }
+
+    @Override
+    public void render(RenderManager rm, ViewPort vp) {
+    }
+
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        throw new IOException("暂不支持");
+    }
+
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        throw new IOException("暂不支持");
+    }
+
+    @Override
+    public Control cloneForSpatial(Spatial spatial) {
+        RotateControl c = new RotateControl(rotateSpeed);
+        c.setSpatial(spatial);
+        return c;
+    }
 }
