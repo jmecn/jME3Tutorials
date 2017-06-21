d1cc0404b7f2397a8251182fccdc2544a8f0add8
jmecn <115050813@qq.com>
2017-06-22 00:10:23 +0800

    改写了dyn4j的例子。

--- src/main/java/net/jmecn/physics2d/BodyControl.java
+++ src/main/java/net/jmecn/physics2d/BodyControl.java
@@ -10,42 +10,42 @@ import com.jme3.scene.control.AbstractControl;
 import com.jme3.util.TempVars;
 
 /**
- * 控制
+ * 物体控制器
  * 
  * @author yanmaoyuan
  *
  */
 public class BodyControl extends AbstractControl {
 
-	private Body body;
+    private Body body;
 
-	public BodyControl(final Body body) {
-		this.body = body;
-	}
+    public BodyControl(final Body body) {
+        this.body = body;
+    }
 
-	public Body getBody() {
-		return body;
-	}
+    public Body getBody() {
+        return body;
+    }
 
-	@Override
-	protected void controlUpdate(float tpf) {
-		TempVars temp = TempVars.get();
-		
-		float x = (float) body.getTransform().getTranslationX();
-		float y = (float) body.getTransform().getTranslationY();
-		float z = spatial.getLocalTranslation().z;
-		spatial.setLocalTranslation(x, y, z);
+    @Override
+    protected void controlUpdate(float tpf) {
+        TempVars temp = TempVars.get();
 
-		Quaternion rotation = temp.quat1;
-		float angle = (float) body.getTransform().getRotation();
-		rotation.fromAngleNormalAxis(angle, Vector3f.UNIT_Z);
-		spatial.setLocalRotation(rotation);
-		
-		temp.release();
-	}
+        float x = (float) body.getTransform().getTranslationX();
+        float y = (float) body.getTransform().getTranslationY();
+        float z = spatial.getLocalTranslation().z;
+        spatial.setLocalTranslation(x, y, z);
 
-	@Override
-	protected void controlRender(RenderManager rm, ViewPort vp) {
-	}
+        Quaternion rotation = temp.quat1;
+        float angle = (float) body.getTransform().getRotation();
+        rotation.fromAngleNormalAxis(angle, Vector3f.UNIT_Z);
+        spatial.setLocalRotation(rotation);
+
+        temp.release();
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+    }
 
 }
--- src/main/java/net/jmecn/physics2d/TestDyn4j.java
+++ src/main/java/net/jmecn/physics2d/TestDyn4j.java
@@ -6,122 +6,159 @@ import org.dyn4j.dynamics.World;
 import org.dyn4j.geometry.Circle;
 import org.dyn4j.geometry.MassType;
 import org.dyn4j.geometry.Rectangle;
-import org.dyn4j.geometry.Vector2;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Sphere;
 
-import net.jmecn.state.AxisAppState;
-
+/**
+ * 演示Dyn4j物理引擎
+ * 
+ * @author yanmaoyuan
+ *
+ */
 public class TestDyn4j extends SimpleApplication {
-	/** The dynamics engine */
-	protected World world;
 
-	@Override
-	public void simpleInitApp() {
-		stateManager.attach(new AxisAppState());
+    // Dyn4j的物理世界
+    private World world = new World();
 
-		cam.setLocation(new Vector3f(5.693723f, 11.948935f, 20.422218f));
-		cam.setRotation(new Quaternion(-0.030991483f, 0.96380556f, -0.23111638f, -0.12924244f));
-		
-		viewPort.setBackgroundColor(ColorRGBA.White);
-		
-		rootNode.addLight(new DirectionalLight(new Vector3f(-1, -2, -3)));
+    @Override
+    public void simpleInitApp() {
+        // 调整摄像机的位置，便于观察场景。
+        cam.setLocation(new Vector3f(-5.326285f, 13.811526f, 22.174831f));
+        cam.setRotation(new Quaternion(0.024544759f, 0.9620277f, -0.2556783f, 0.09235176f));
 
-		// create the world
-		this.world = new World();
+        // 背景色改成白色
+        viewPort.setBackgroundColor(ColorRGBA.White);
 
-		floor();
-		
-		createCircle(0.5f);
-		
-		rectangle();
-	}
+        // 添加光源
+        rootNode.addLight(new DirectionalLight(new Vector3f(-1, -2, -3).normalizeLocal()));
 
-	private Material getMaterial() {
-		ColorRGBA color = ColorRGBA.randomColor();
-		
-		Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		mat.setColor("Diffuse", color);
-		mat.setColor("Ambient", color.mult(0.7f));
-		mat.setColor("Specular", ColorRGBA.Black);
-		mat.setFloat("Shininess", 1f);
-		mat.setBoolean("UseMaterialColors", true);
-		return mat;
-	}
-	
-	private void floor() {
-		// create the floor
-		Rectangle floorRect = new Rectangle(15.0, 1.0);
-		Body floor = new Body();
-		floor.addFixture(new BodyFixture(floorRect));
-		floor.setMass(MassType.INFINITE);
-		this.world.addBody(floor);
+        makeFloor();
 
-		// create spatial
-		Box box = new Box(7.5f, 0.5f, 1f);
-		Geometry geom = new Geometry("body", box);
-		geom.setMaterial(getMaterial());
-		geom.addControl(new BodyControl(floor));
-		rootNode.attachChild(geom);
-	}
+        makeCircle();
 
-	private void createCircle(float radius) {
-		// create a circle
-		Circle cirShape = new Circle(radius);
-		Body circle = new Body();
-		circle.addFixture(cirShape);
-		circle.setMass(MassType.NORMAL);
-		circle.translate(2.0, 10.0);
-		// test adding some force
-		circle.applyForce(new Vector2(-100.0, 0.0));
-		// set some linear damping to simulate rolling friction
-		circle.setLinearDamping(0.05);
-		this.world.addBody(circle);
+        makeRectangle();
+    }
 
-		// create a sphere
-		Sphere sphere = new Sphere(12, 12, radius);
-		Geometry geom = new Geometry("body", sphere);
-		geom.setMaterial(getMaterial());
-		geom.addControl(new BodyControl(circle));
-		rootNode.attachChild(geom);
-	}
+    /**
+     * 创建随机颜色的感光材质
+     * 
+     * @return
+     */
+    private Material getMaterial() {
+        ColorRGBA color = ColorRGBA.randomColor();
 
-	private void rectangle() {
-		// try a rectangle
-		Rectangle rectShape = new Rectangle(1.0, 1.0);
-		Body rectangle = new Body();
-		rectangle.addFixture(rectShape);
-		rectangle.setMass(MassType.NORMAL);
-		rectangle.translate(0.0, 6.0);// 上移6个单位
-		rectangle.getLinearVelocity().set(-5.0, 0.0);// 线速度
-		rectangle.setAngularVelocity(-FastMath.PI);// 角速度
-		this.world.addBody(rectangle);
+        Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        mat.setColor("Diffuse", color);
+        mat.setColor("Ambient", color.mult(0.7f));
+        mat.setColor("Specular", ColorRGBA.Black);
+        mat.setFloat("Shininess", 1f);
+        mat.setBoolean("UseMaterialColors", true);
+        return mat;
+    }
 
-		// create a rectangle
-		Box rect = new Box(0.5f, 0.5f, 0.5f);
-		Geometry geom = new Geometry("body", rect);
-		geom.setMaterial(getMaterial());
-		geom.addControl(new BodyControl(rectangle));
-		rootNode.attachChild(geom);
-	}
+    /**
+     * 建造一个固定的“地板”
+     */
+    private void makeFloor() {
+        // 矩形的高和宽
+        float width = 28f;
+        float height = 0.1f;
 
-	@Override
-	public void simpleUpdate(float tpf) {
-		this.world.update(tpf);
-	}
+        // 刚体
+        Body body = new Body();
+        body.addFixture(new Rectangle(width, height));// 碰撞形状
+        body.setMass(MassType.INFINITE);// 质量无穷大
 
-	public static void main(String[] args) {
-		TestDyn4j app = new TestDyn4j();
-		app.start();
-	}
+        world.addBody(body);
+
+        // 几何体
+        // 2D的物体并没有厚度，但jME3毕竟是一个3D引擎。所以也可以用3D方块来表示2D的矩形。 :)
+        Geometry geom = new Geometry("body", new Box(width / 2, height / 2, 0.1f));
+        geom.setMaterial(getMaterial());
+        
+        // 绑定刚体和模型
+        geom.addControl(new BodyControl(body));
+
+        rootNode.attachChild(geom);
+    }
+
+    /**
+     * 制造一个圆形
+     */
+    private void makeCircle() {
+        // 半径
+        float radius = 0.123f;
+
+        // 刚体
+        Body body = new Body();
+
+        // 碰撞形状
+        BodyFixture fixture = body.addFixture(new Circle(radius));
+        fixture.setRestitution(0.7);// 弹性
+
+        body.setMass(MassType.NORMAL);// 普通质量
+        body.translate(-10, 1);// 位置坐标
+        body.setLinearVelocity(8, 5);// 线速度
+        body.setLinearDamping(0.05);// 阻尼
+
+        world.addBody(body);
+        
+        // 几何体
+        Geometry geom = new Geometry("body", new Sphere(32, 32, radius));
+        geom.setMaterial(getMaterial());
+
+        // 绑定刚体和模型
+        geom.addControl(new BodyControl(body));
+
+        rootNode.attachChild(geom);
+    }
+
+    /**
+     * 制造矩形木板
+     */
+    private void makeRectangle() {
+        float width = 0.03f;
+        float height = 1.05f;
+
+        // 生成10个木板，从半场开始，间隔一米摆放。
+        for (int i = 0; i < 10; i++) {
+            // 刚体
+            Body body = new Body();
+            // 碰撞形状
+            body.addFixture(new Rectangle(width, height));
+            body.setMass(MassType.NORMAL);
+            body.translate(i * 1, height / 2);// 设置位置
+
+            world.addBody(body);
+
+            // 几何体
+            Geometry geom = new Geometry("body", new Box(width / 2, height / 2, 0.1f));
+            geom.setMaterial(getMaterial());
+
+            // 绑定刚体和模型
+            geom.addControl(new BodyControl(body));
+
+            rootNode.attachChild(geom);
+        }
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        // 更新Dyn4j物理世界
+        world.update(tpf);
+    }
+
+    public static void main(String[] args) {
+        TestDyn4j app = new TestDyn4j();
+        app.start();
+    }
 
 }
