49152b54f9d1446fdf96e1a0fb086039b7c2c490
yan <115050813@qq.com>
2017-06-28 01:52:16 +0800

    增加多种材质效果

--- src/main/java/net/jmecn/outscene/TestTerrain.java
+++ src/main/java/net/jmecn/outscene/TestTerrain.java
@@ -2,7 +2,9 @@ package net.jmecn.outscene;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
+import com.jme3.terrain.geomipmap.TerrainLodControl;
 import com.jme3.terrain.geomipmap.TerrainQuad;
+import com.jme3.terrain.geomipmap.lodcalc.DistanceLodCalculator;
 import com.jme3.terrain.heightmap.ImageBasedHeightMap;
 import com.jme3.texture.Texture;
 
@@ -18,10 +20,6 @@ public class TestTerrain extends SimpleApplication {
     public void simpleInitApp() {
         flyCam.setMoveSpeed(100);
 
-        // 加载材质
-        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        material.getAdditionalRenderState().setWireframe(true);
-
         // 加载高度图
         Texture heightMapImage = assetManager.loadTexture("Scenes/Maps/DefaultMap/default.png");
 
@@ -34,11 +32,21 @@ public class TestTerrain extends SimpleApplication {
         // 该地形被分解成边长65(64*64)的矩形区块，用于优化网格。
         // 高度图的边长为 257，分辨率 256*256。
         TerrainQuad terrain = new TerrainQuad("heightmap", 65, 257, heightData);
-        terrain.setMaterial(material);
-        terrain.center();
-
         rootNode.attachChild(terrain);
+        terrain.center();
+        
+        // 加载材质
+        Material material = assetManager.loadMaterial("Scenes/Maps/DefaultMap/default_height_based.j3m");
+        
+        terrain.setMaterial(material);
 
+        // 层次细节（LOD）优化
+        TerrainLodControl lodControl = new TerrainLodControl(terrain, cam);
+        // LOD计算器，一个参数代表区块大小，第二个参数代表距离系数。
+        // size = 65, multiplier = 2.7f, distance = 65 * 2.7f
+        lodControl.setLodCalculator(new DistanceLodCalculator(65, 2.7f));
+        
+        terrain.addControl(lodControl);
     }
 
     public static void main(String[] args) {
